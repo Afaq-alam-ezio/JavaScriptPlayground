e257929937d9de498786a232b831f7ee2c5494bb
Afaq-alam <afaqalam323@gmail.com>
2025-02-24 22:10:57 +0530

    Added the 2nd file

---
+++ 01_DataTypes.js
@@ -0,0 +1,18 @@
+const userId = 103;
+
+var userName = "Afaq";
+
+let userAge = 21;
+
+userEmail = "Afaq@gmail.com";
+
+let userSalary;        
+
+// "var" is now depriciated method replaced by "let"
+/* var is not scope dependent, meaning it will act like always a global variable, thus we don't use it.*/
+
+// userId = 213; can't change cuz its is of constant type of datatype
+
+console.log(userId);
+
+console.table([userId, userName, userAge, userEmail, userSalary]);
