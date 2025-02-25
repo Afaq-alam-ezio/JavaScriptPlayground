948239f42b9c684557688104e729687262745175
Afaq-alam <afaqalam323@gmail.com>
2025-02-25 13:25:50 +0530

    Added new file 3

---
+++ 03_dataTypeConversion.js
@@ -0,0 +1,41 @@
+// EXAMPLE 1
+// let score = 33;
+// let value = String(score);
+// console.log(typeof(score));
+// console.log(typeof(value));
+// console.log(value);
+
+// EXAMPLE 2
+// let score = "33";
+// let value = Number(score);
+// console.log(typeof(score));
+// console.log(typeof(value));
+// console.log(value);
+
+// EXAMPLE 3
+// let score = null;
+// let value = Number(score);
+// console.log(typeof(score));
+// console.log(typeof(value));
+// console.log(value);
+
+// EXAMPLE 4
+// let score = undefined;
+// let value = Number(score);
+// console.log(typeof(score));
+// console.log(typeof(value));
+// console.log(value);
+
+// EXAMPLE 5
+// let score = null;
+// let value = String(score);
+// console.log(typeof(score));
+// console.log(typeof(value));
+// console.log(value);             * value is "null" here.
+
+// EXAMPLE 6
+let score = "Afaq";
+let value = Number(score);
+console.log(typeof(score));
+console.log(typeof(value));
+console.log(value);
