caf670a51209a0d7a5627cfe57fcc1e86d3abbd6
Afaq-alam <afaqalam323@gmail.com>
2025-03-01 20:45:25 +0530

    Added file 13

---
+++ 13_Dates.js
@@ -0,0 +1,11 @@
+let myDate = new Date();
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+
+let date = new Date(2023, 0, 1);
+console.log(date.toDateString());
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
